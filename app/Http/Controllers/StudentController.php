ff3b5f7bbe17ad380f33700757781aae8c0a6f02
Mona <moon.2060@gmail.com>
2024-09-28 22:14:42 +0300

    'second commit'

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorestudentRequest;
+use App\Http\Requests\UpdatestudentRequest;
+use App\Models\student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorestudentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatestudentRequest $request, student $student)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(student $student)
+    {
+        //
+    }
+}
